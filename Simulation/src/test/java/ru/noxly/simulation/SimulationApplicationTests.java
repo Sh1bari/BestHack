0bdd37991a5eaab3029e888f75eead9720fe4b18
Sh1bari <vova_krasnov_2004@mail.ru>
2025-03-07 23:05:36 +0300

    Init

---
+++ Simulation/src/test/java/ru/noxly/simulation/SimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.noxly.simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
